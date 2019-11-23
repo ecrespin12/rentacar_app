849388617a00203f8cb569ebdab60bc150391603
daviddlopezz <46541007+daviddlopezz@users.noreply.github.com>
2019-11-23 00:27:08 -0600

    Agregando clases de Auto

---
+++ CapaDatos/D_Auto.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad;
+
+namespace CapaDatos
+{
+    public class D_Auto
+    {
+        //obj para la conexion
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlConexion"].ConnectionString);
+
+
+
+        public DataTable D_ListAuto()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tbl_aut_auto", con);
+            cmd.CommandType = CommandType.Text;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public void D_InsertAuto(E_Auto instancia)
+        {
+            SqlCommand cmd = new SqlCommand("auto_insert_sp", con);
+
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@nombre", instancia.nombre);
+
+            if (con.State == ConnectionState.Open)
+                con.Close();
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void D_ActualizarAuto(E_Auto instancia)
+        {
+            SqlCommand cmd = new SqlCommand("auto_actualizar_sp", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@nombre", instancia.nombre);
+            cmd.Parameters.AddWithValue("@id", instancia.codigo);
+
+            if (con.State == ConnectionState.Open)
+                con.Close();
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void D_EliminarAuto(E_Auto instancia)
+        {
+            SqlCommand cmd = new SqlCommand("auto_eliminar_sp", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", instancia.codigo);
+
+            if (con.State == ConnectionState.Open)
+                con.Close();
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public DataSet D_GetAuto(int codigo)
+        {
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tbl_aut_auto WHERE tia_codigo = @codigo", con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+
+
+            DataSet ds = null;
+            try
+            {
+                ds = new DataSet();
+
+                cmd.CommandTimeout = 600;
+
+                cmd.CommandType = CommandType.Text;
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                da.Fill(ds);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            con.Close();
+            return ds;
+        }
+
+
+    }
+}
---
+++ CapaNegocio/N_Auto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class N_Auto
+    {
+        D_Auto dAuto = new D_Auto();
+
+        public DataTable N_ListAuto()
+        {
+            return dAuto.D_ListAuto();
+        }
+
+        public void InsertAuto(E_Auto instancia)
+        {
+
+            dAuto.D_InsertAuto(instancia);
+        }
+
+        public void ActualizarAuto(E_Auto instancia)
+        {
+
+            dAuto.D_ActualizarAuto(instancia);
+        }
+
+        public void EliminarTipoAuto(E_Auto instancia)
+        {
+            dAuto.D_EliminarAuto(instancia);
+        }
+
+        public DataSet GetAuto(int codigo)
+        {
+            return dAuto.D_GetAuto(codigo);
+        }
+    }
+}
